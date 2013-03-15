06fe8fb9571b3ce0d81e8e28e788cbb4e918ceb2
Trevor Turk <trevorturk@gmail.com>
2013-03-15 17:40:53 -0500

    Blank 3.2.12 generate with --skip-active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCookieIssue::Application.config.secret_token = '37851896b0d5522271049f33ca601e3283a913b406168fe51a162ff3bf4e42e7e11425601cd00c736f26c63b6efc60b8c7fcd9b3fa021a92347f2c06919ad53b'
