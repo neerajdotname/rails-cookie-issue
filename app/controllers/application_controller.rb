7c913491d01bd0bba6f9c738af2e58f85b0e64b4
Trevor Turk <trevorturk@gmail.com>
2013-03-15 17:56:05 -0500

    Step 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def index
+    session[:test] ||= 'success'
+    cookies.signed[:test] ||= 'success'
+    render text: "session[:test] = #{session[:test]} ; cookies.signed[:test] = #{cookies.signed[:test]}"
+  end
 end
